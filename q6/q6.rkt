3cb70ac4ab115d9cf5f96cafc2bfd9337a76cf55
Daniel Hannon <12559951+danielh2942@users.noreply.github.com>
2021-12-06 14:05:28 +0000

    Fixed Style of Day 6 and made small optimization
    Thank You anon for sending me the style guide :)

--- q6/q6.rkt
+++ q6/q6.rkt
@@ -1,7 +1,6 @@
 #lang racket
 (define (string_list->num_list input)
-  (if (null? input) '() (append (list (string->number(string-trim (car input)))) (string_list->num_list (cdr input))))
-  )
+  (if (null? input) '() (cons (string->number(string-trim (car input))) (string_list->num_list (cdr input)))))
 
 (define (count_into_ht input ht)
   (if (null? input)
@@ -9,12 +8,8 @@
       (begin
         (if (not (hash-ref ht (car input) #f))
             (hash-set! ht (car input) 1)
-            (hash-set! ht (car input) (+ (hash-ref ht (car input)) 1))
-            )
-        (count_into_ht (cdr input) ht)
-        )
-      )
-  )
+            (hash-set! ht (car input) (+ (hash-ref ht (car input)) 1)))
+        (count_into_ht (cdr input) ht))))
 
 (define (get_next_day_hash ht)
   (define new_ht (make-hash))
@@ -23,40 +18,28 @@
       (if (= i 0)
           (begin
             (hash-set! new_ht 6 (hash-ref ht i))
-            (hash-set! new_ht 8 (hash-ref ht i))
-          )
+            (hash-set! new_ht 8 (hash-ref ht i)))
           (begin
             (if (not (hash-ref new_ht (- i 1) #f))
                 (hash-set! new_ht (- i 1) (hash-ref ht i))
-                (hash-set! new_ht (- i 1) (+ (hash-ref ht i) (hash-ref new_ht (- i 1))))
-                )
-            )
-          )
-      )
-    )
-  new_ht
-  )
+                (hash-set! new_ht (- i 1) (+ (hash-ref ht i) (hash-ref new_ht (- i 1)))))))))
+  new_ht)
+
 (define (increment_until_hash ht days)
   (define (increment_until_hash_inner ht current days)
     (if (= current days)
         ht
-        (increment_until_hash_inner (get_next_day_hash ht) (+ current 1) days)
-        )
-    )
-  (increment_until_hash_inner ht 0 days)
-  )
-(define data (string_list->num_list (string-split (car (file->lines "input.txt")) ",")))
+        (increment_until_hash_inner (get_next_day_hash ht) (+ current 1) days)))
+  (increment_until_hash_inner ht 0 days))
 
 (define (count_total ht)
   (define tally 0)
   (for ([i (in-range 0 9)])
     (when (hash-ref ht i #f)
-      (set! tally (+ tally (hash-ref ht i)))
-      )
-    )
-  tally
-  )
+      (set! tally (+ tally (hash-ref ht i)))))
+  tally)
 
+(define data (string_list->num_list (string-split (car (file->lines "input.txt")) ",")))
 (set! data (sort data <))
 (define ht (make-hash))
 (count_into_ht data ht)
