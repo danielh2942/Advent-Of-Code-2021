aeca4b67dfeb502cd22a0a3216798722aa6b8bb7
Daniel Hannon <12559951+danielh2942@users.noreply.github.com>
2021-12-08 21:56:17 +0000

    Fixed Style of Day 4

--- q4/q4.rkt
+++ q4/q4.rkt
@@ -16,11 +16,7 @@
       (begin
         (if (= index 0)
             (car search_list)
-            (nth (- index 1) (cdr search_list))
-            )
-        )
-      )
-  )
+            (nth (- index 1) (cdr search_list))))))
 
 (define (nthcdr index search_list)
   (if (null? search_list)
@@ -28,18 +24,12 @@
       (begin
         (if (= index 0)
             search_list
-            (nthcdr (- index 1) (cdr search_list))
-            )
-        )
-      )
-  )
+            (nthcdr (- index 1) (cdr search_list))))))
 
 (define (nthcol index search_list)
   (if (null? search_list)
       '()
-      (append (list (nth index (car search_list))) (nthcol index (cdr search_list)))
-      )
-  )
+      (append (list (nth index (car search_list))) (nthcol index (cdr search_list)))))
 
 (define (xuntily from until search_list)
   (if (null? search_list)
@@ -49,21 +39,13 @@
             (begin
               (if (> until 0)
                   (append (list (car search_list)) (xuntily from (- until 1) (cdr search_list)))
-                  (list (car search_list))
-                  )
-              )
-            (xuntily (- from 1) (- until 1) (cdr search_list))
-            )
-        )
-      )
-  )
+                  (list (car search_list))))
+            (xuntily (- from 1) (- until 1) (cdr search_list))))))
 
 (define (numlisttoints input)
   (if (null? input)
       '()
-      (append (list (string->number (car input))) (numlisttoints (cdr input)))
-      )
-  )
+      (append (list (string->number (car input))) (numlisttoints (cdr input)))))
 
 (define (numliststoints input)
   (if (null? input)
@@ -71,33 +53,20 @@
       (begin
         (if (pair? input)
             (append (list (numlisttoints (string-split (car input)))) (numliststoints (cdr input)))
-            (list (numlisttoints (string-split input)))
-            )
-        )
-      )
-  )
+            (list (numlisttoints (string-split input)))))))
 
 (define (generate_bingo_card input)
   (if (null? input)
       '()
-      (append (list (numliststoints (xuntily 1 5 input))) (generate_bingo_card (nthcdr 6 input)))
-      )
-  )
+      (append (list (numliststoints (xuntily 1 5 input))) (generate_bingo_card (nthcdr 6 input)))))
 
 (define (contains_sublist sublist discrim)
   (define (contains_sublist_inner sublist discrim count)
     (if (null? sublist)
         count
         (begin
-          (if (pair? (member (car sublist) discrim))
-            (contains_sublist_inner (cdr sublist) discrim (+ count 1))
-            0
-            )
-          )
-        )
-    )
-  (contains_sublist_inner sublist discrim 0)
-  )
+          (if (pair? (member (car sublist) discrim)) (contains_sublist_inner (cdr sublist) discrim (+ count 1)) 0))))
+  (contains_sublist_inner sublist discrim 0))
 
 (define (check_for_bingo card nums_called)
   (define (check_for_bingo_inner card nums_called index)
@@ -106,13 +75,8 @@
         (begin
           (if (or (= (contains_sublist (nth index card) nums_called) 5) (= (contains_sublist (nthcol index card) nums_called) 5))
               card
-              (check_for_bingo_inner card nums_called (+ index 1))
-              )
-          )
-        )
-    )
-  (check_for_bingo_inner card nums_called 0)
-  )
+              (check_for_bingo_inner card nums_called (+ index 1))))))
+  (check_for_bingo_inner card nums_called 0))
 
 (define (check_for_winners cards nums_called)
   (if (null? cards)
@@ -120,11 +84,7 @@
       (begin
         (if (not (null? (check_for_bingo (car cards) nums_called)))
             (car cards)
-            (check_for_winners (cdr cards) nums_called)
-            )
-        )
-      )
-  )
+            (check_for_winners (cdr cards) nums_called)))))
 
 (define (remove_elems input remove)
   (define (remove_elems_inner input remove)
@@ -133,20 +93,13 @@
       (begin
         (if (member (car input) remove)
             (remove_elems_inner (cdr input) remove)
-            (append (list (car input)) (remove_elems_inner (cdr input) remove))
-            )
-        )
-      )
-    )
-  (remove_elems_inner (sort (flatten input) <) (sort remove <))
-  )
+            (append (list (car input)) (remove_elems_inner (cdr input) remove))))))
+  (remove_elems_inner (sort (flatten input) <) (sort remove <)))
 
 (define (sum_list L)
   (if (null? L)
       0
-      (+ (car L) (sum_list (cdr L)))
-      )
-  )
+      (+ (car L) (sum_list (cdr L)))))
 
 (define (bingo_loop cards discrim)
   (define (bingo_loop_inner cards discrim amount_called)
@@ -157,13 +110,9 @@
            (display (xuntily 0 amount_called discrim))
            (display "\n")
            (display (* (nth amount_called discrim) (sum_list (remove_elems (flatten winner) (xuntily 0 amount_called discrim)))))
-           (display "\n")
-          )
-        (bingo_loop_inner cards discrim (+ amount_called 1))
-        )
-    )
-  (bingo_loop_inner cards discrim 4)
-  )
+           (display "\n"))
+        (bingo_loop_inner cards discrim (+ amount_called 1))))
+  (bingo_loop_inner cards discrim 4))
 
 (define (bingo_loop_last cards discrim)
   (define (bingo_loop_last_inner cards discrim amount_called)
@@ -177,16 +126,10 @@
                 (display "\n")
                 (display (* (nth amount_called discrim) (sum_list (remove_elems (flatten winner) (xuntily 0 amount_called discrim)))))
                 (display "\n")
-                amount_called
-                )
-              amount_called
-          )
-          )
-        (bingo_loop_last_inner cards discrim (+ amount_called 1))
-        )
-    )
-  (bingo_loop_last_inner cards discrim 4)
-  )
+                amount_called)
+              amount_called))
+        (bingo_loop_last_inner cards discrim (+ amount_called 1))))
+  (bingo_loop_last_inner cards discrim 4))
 
 (define (get_last_winner cards discrim highest)
   (define temp 0)
@@ -196,11 +139,7 @@
         (set! temp (bingo_loop_last (list (car cards)) discrim))
         (if (and (not (void? temp)) (> temp highest))
             (get_last_winner (cdr cards) discrim temp)
-            (get_last_winner (cdr cards) discrim highest)
-            )
-        )
-      )
-  )
+            (get_last_winner (cdr cards) discrim highest)))))
 
 (define bingo_text (file-reader "input.txt"))
 (define bingo_cards (generate_bingo_card bingo_text))
